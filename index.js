2ba95dd294cb3a09bba8724fd3380c70db6c99b6
Julio Martelosso <martelossomartin1506.student@ifosupwavre.be>
2017-11-24 17:42:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -165,7 +165,7 @@ function moveDodger(e) {
        moveDodgerLeft()
      }
      else if (e.which === RIGHT_ARROW) {
-       moveDodgerRight()
+       window.requestAnimationFrame(moveDodgerRight())
      }
    })
 
